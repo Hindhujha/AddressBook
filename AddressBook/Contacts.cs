48d4eda7e27b7bac6c5eec34907d625a3fc73fb7
unknown <hindhujhasridhar@gmail.com>
2021-12-31 19:49:42 +0530

    CREATING A CONTACT

---
+++ AddressBook/Contacts.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public  class Contacts
+    {
+      //Creating A Contact by get the input from USER
+        public  void CreateContact()
+        {
+
+            Person person = new Person();
+
+            Console.WriteLine("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+  
+            Console.WriteLine("Enter Address: ");
+            person.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter City: ");
+            person.City = Console.ReadLine();
+
+            Console.WriteLine("Enter State: ");
+            person.State =Console.ReadLine();
+
+            Console.WriteLine("Enter Zip Code: ");
+            person.Zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone Number: ");
+            person.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter Email Id: ");
+            person.email = Console.ReadLine();
+
+            Console.WriteLine("-------------------------------------------");
+
+            Contacts show = new Contacts();
+
+            show.Display(person);
+
+          
+        }
+        //Display the contacts
+        public  void Display(Person person)
+        {
+            Console.WriteLine("PERSON DETAILS ARE:");
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Address : " + person);
+            Console.WriteLine("City: " + person.City);
+            Console.WriteLine("State: " + person.State);
+            Console.WriteLine("Zip Code: " + person.Zip);
+            Console.WriteLine("Phone Number: " + person.PhoneNumber);
+            Console.WriteLine("Email Id : " + person.email);
+            Console.WriteLine("-------------------------------------------");
+        }
+
+      
+    }
+
+    //Declaring datatype for Person
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public int Zip { get; set; }
+        public long PhoneNumber { get; set; }
+        public string email { get; set; }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 
 namespace AddressBook
 {
-    class Program
+    class Program:Contacts
     {
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO ADDRESS BOOK PROBLEMS");
+
+            Console.WriteLine("-----------------");
+            Console.WriteLine("1.CREATING ADDRESS BOOK");
+            Console.WriteLine("2.ADDING NEW CONTACT IN ADDRESS BOOK");
+            Console.WriteLine("ENTER YOUR OPTION");
+
+            int select = Convert.ToInt32(Console.ReadLine());
+
+            switch (select)
+            {
+                case 1:
+                    Contacts create = new Contacts();
+                    create.CreateContact();
+               
+                    break;
+                default:
+                    Console.WriteLine("ENTER A CORRECT OPTION");
+                    Console.ReadLine();
+                    break;
+            }
         }
     }
 }
