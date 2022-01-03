9ff8edf456eba650ebc8ddff2a14130e143771f0
unknown <hindhujhasridhar@gmail.com>
2022-01-03 15:55:11 +0530

    CONTACT EDITED IN ADDRESS BOOK

--- AddressBook/Contacts.cs
+++ AddressBook/Contacts.cs
@@ -8,7 +8,7 @@ namespace AddressBook
 {
     public class Contacts:Person
     {
-        public List<Person> People = new List<Person>();
+  
 
 
         //Creating A Contact by get the input from USER
@@ -89,13 +89,58 @@ namespace AddressBook
         public void EditContact()
         {
 
-            Contacts create = new Contacts();
-           
-            create.AddingContact();
-            Console.WriteLine("ENTER A NAME TO EDIT CONTACT");
+
+            Console.WriteLine("CONTACTS IN ADDRESS BOOK ARE :");
+            Contacts show = new Contacts();
+            show.AddressBook();
+      
+            foreach (var person in People)
+            {
+                show.Display(person);
+            }
+            Console.WriteLine("ENTER A PERSONS FIRST NAME TO EDIT THE DETAILS");
             string name = Console.ReadLine();
-            //Person person = new Person();
-            if (FirstName == name)
+            Person person1 = new Person();
+            if (name == "REENA")
+            {
+                Console.WriteLine("ENTER A DETAIL TO EDIT:1.City 2.State 3.Zip");
+                int select = Convert.ToInt32(Console.ReadLine());
+                
+                switch (select)
+                {
+                          
+                    case 1:
+                      
+                        Console.WriteLine("ENTER A CITY NAME");
+                        string city = Console.ReadLine();
+                        person1.City = city;
+                       
+                        break;
+                    case 2:
+                   
+                        Console.WriteLine("ENTER A STATE NAME");
+                        string state = Console.ReadLine();
+                        person1. State = state;
+                        break;
+                    case 3:
+                        Console.WriteLine("ENTER A STATE NAME");
+                        int zip = Convert.ToInt32(Console.ReadLine());
+                        person1.Zip = zip;
+                        break;
+                    default:
+                        Console.WriteLine("ENTER CORRECT OPTION");
+                        break;
+
+                }
+                Console.WriteLine("AFTER EDITING CONTACTS IN ADDRESS BOOK");
+                foreach (var person in People)
+                {
+                    Contacts book = new Contacts();
+                    book.AddressBook();
+                    show.Display(person);
+                }
+            }
+            else if (name == "SITA")
             {
                 Console.WriteLine("ENTER A DETAIL TO EDIT:1.City 2.State 3.Zip");
                 int select = Convert.ToInt32(Console.ReadLine());
@@ -105,32 +150,83 @@ namespace AddressBook
                     case 1:
                         Console.WriteLine("ENTER A CITY NAME");
                         string city = Console.ReadLine();
-                        City = city;
+                        person1.City = city;
                         break;
                     case 2:
                         Console.WriteLine("ENTER A STATE NAME");
                         string state = Console.ReadLine();
-                        State = state;
+                        person1.State = state;
                         break;
                     case 3:
                         Console.WriteLine("ENTER A STATE NAME");
                         int zip = Convert.ToInt32(Console.ReadLine());
-                        Zip = zip;
+                        person1.Zip = zip;
                         break;
                     default:
                         Console.WriteLine("ENTER CORRECT OPTION");
                         break;
-
                 }
-                Contacts edited = new Contacts();
-              
-                edited.AddingContact();
+                Console.WriteLine("AFTER EDITING CONTACTS IN ADDRESS BOOK");
+                foreach (var person in People)
+                {
+                    show.Display(person);
+                }
             }
         }
-    }
-        //Declaring datatype for Person
-        public class Person
+
+        //CONTACTS IN ADDRESS BOOK 
+        public static List<Person> People = new List<Person>();
+
+        public void Add(Person person)
         {
+            People.Add(person);
+        }
+
+        public void AddressBook()
+        {
+            Contacts create = new Contacts();
+            create.Add(new Person()
+            {
+                FirstName = "REENA",
+                LastName = "THOMAS",
+                Address = "SK NAGAR",
+                City = "SALEM",
+                State = "TAMILNADU",
+                Zip = 636402,
+                PhoneNumber = 9992345613,
+                email = "reena@gmail.com"
+
+            });
+            create.Add(new Person()
+            {
+                FirstName = "SITA",
+                LastName = "DANIEL",
+                Address = "PO NAGAR",
+                City = "BANGALORE",
+                State = "KARNATAKA",
+                Zip = 636700,
+                PhoneNumber = 9992345983,
+                email = "sitadanie@gmail.com"
+
+            });
+            create.Add(new Person()
+            {
+                FirstName = "RAM",
+                LastName = "KUMAR",
+                Address = "AK NAGAR",
+                City = "CHENNAI",
+                State = "TAMILNADU",
+                Zip = 632007,
+                PhoneNumber = 9992345603,
+                email = "ram1234@gmail.com"
+            });
+        }
+
+    }
+
+    //Declaring datatype for Person
+    public class Person
+    {
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public string Address { get; set; }
@@ -140,6 +236,8 @@ namespace AddressBook
             public int Zip { get; set; }
             public long PhoneNumber { get; set; }
             public string email { get; set; }
-        }
-    }
+   
+    } 
+}
+
 
